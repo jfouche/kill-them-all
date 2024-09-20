eb2c073691238d8ce7528bcfd773c13bcc051871
Jérémie Fouché <jeremie.fouche@gmail.com>
2024-09-20 16:37:08 +0200

    refactored UpgradeProvider to use RngKindProvider

--- src/components/upgrade.rs
+++ src/components/upgrade.rs
@@ -1,8 +1,9 @@
-use rand::{rngs::ThreadRng, seq::IteratorRandom, Rng};
-use std::collections::HashMap;
+use super::rng_provider::{Generator, RngKindProvider};
+use bevy::prelude::*;
+use rand::{rngs::ThreadRng, Rng};
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-enum UpgradeType {
+pub enum UpgradeKind {
     IncreaseMaxLife,
     IncreaseLifeRegen,
     IncreaseAttackSpeed,
@@ -10,20 +11,20 @@ enum UpgradeType {
     Pierce,
 }
 
-impl UpgradeType {
-    fn gen(&self, rng: &mut ThreadRng) -> Upgrade {
+impl Generator<Upgrade> for UpgradeKind {
+    fn generate(&self, rng: &mut ThreadRng) -> Upgrade {
         match self {
-            UpgradeType::IncreaseMaxLife => Upgrade::IncreaseMaxLife(rng.gen_range(2..10) as f32),
-            UpgradeType::IncreaseLifeRegen => {
+            UpgradeKind::IncreaseMaxLife => Upgrade::IncreaseMaxLife(rng.gen_range(2..10) as f32),
+            UpgradeKind::IncreaseLifeRegen => {
                 Upgrade::IncreaseLifeRegen(rng.gen_range(2..10) as f32)
             }
-            UpgradeType::IncreaseAttackSpeed => {
+            UpgradeKind::IncreaseAttackSpeed => {
                 Upgrade::IncreaseAttackSpeed(rng.gen_range(2..20) as f32)
             }
-            UpgradeType::IncreasemovementSpeed => {
+            UpgradeKind::IncreasemovementSpeed => {
                 Upgrade::IncreasemovementSpeed(rng.gen_range(2..20) as f32)
             }
-            UpgradeType::Pierce => Upgrade::Pierce(rng.gen_range(2..20) as f32),
+            UpgradeKind::Pierce => Upgrade::Pierce(rng.gen_range(2..20) as f32),
         }
     }
 }
@@ -42,44 +43,18 @@ pub enum Upgrade {
     Pierce(f32),
 }
 
-pub struct UpgradeProvider {
-    upgrades: HashMap<UpgradeType, Vec<UpgradeType>>,
-    filters: Vec<UpgradeType>,
-}
+#[derive(Deref, DerefMut)]
+pub struct UpgradeProvider(RngKindProvider<UpgradeKind, Upgrade>);
 
 impl UpgradeProvider {
     pub fn new() -> Self {
-        let mut provider = UpgradeProvider {
-            upgrades: HashMap::new(),
-            filters: vec![],
-        };
-        provider.add(UpgradeType::IncreaseMaxLife, 40);
-        provider.add(UpgradeType::IncreaseLifeRegen, 40);
-        provider.add(UpgradeType::IncreaseAttackSpeed, 20);
-        provider.add(UpgradeType::IncreasemovementSpeed, 40);
-        provider.add(UpgradeType::Pierce, 20);
+        let mut provider = RngKindProvider::<UpgradeKind, Upgrade>::default();
+        provider.add(UpgradeKind::IncreaseMaxLife, 40);
+        provider.add(UpgradeKind::IncreaseLifeRegen, 40);
+        provider.add(UpgradeKind::IncreaseAttackSpeed, 20);
+        provider.add(UpgradeKind::IncreasemovementSpeed, 40);
+        provider.add(UpgradeKind::Pierce, 20);
 
-        provider
-    }
-
-    fn add(&mut self, upgrade: UpgradeType, weight: usize) -> &mut Self {
-        self.upgrades.insert(upgrade, vec![upgrade; weight]);
-        self
-    }
-
-    /// generate a rand upgrade, removing the option the select it next time
-    pub fn gen(&mut self) -> Option<Upgrade> {
-        let mut rng = rand::thread_rng();
-
-        let upgrade_type = self
-            .upgrades
-            .iter()
-            .filter(|(upgrade_type, _v)| !self.filters.contains(upgrade_type))
-            .flat_map(|(_upgrade_type, v)| v)
-            .choose(&mut rng)?;
-
-        self.filters.push(*upgrade_type);
-        let upgrade = upgrade_type.gen(&mut rng);
-        Some(upgrade)
+        UpgradeProvider(provider)
     }
 }
