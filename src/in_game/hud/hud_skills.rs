0f1e3471b49066244a0816694c659d015b1a7e3d
Jérémie Fouché <jeremie.fouche@gmail.com>
2025-03-11 13:01:46 +0100

    better inventory button in hud

--- src/in_game/hud/hud_skills.rs
+++ src/in_game/hud/hud_skills.rs
@@ -7,7 +7,6 @@ use crate::{
     },
     in_game::menu::popup_info::SpawnInfoPopupObservers,
     schedule::GameState,
-    ui::button::TextButton,
     utils::observers::VecObserversExt,
 };
 use bevy::prelude::*;
@@ -28,20 +27,39 @@ impl Plugin for HudSkillsPlugin {
         position_type: PositionType::Absolute,
         right: Val::Px(50.),
         bottom: Val::Px(15.),
-        height: Val::Px(32.),
+        height: Val::Px(48.),
         ..Default::default()
     })
 )]
 struct HudSkillsPanel;
 
+#[derive(Component)]
+#[require(
+    Button,
+    Node(|| Node {
+        width: Val::Px(50.0),
+        height: Val::Percent(100.),
+        margin: UiRect::all(Val::Px(5.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        border: UiRect::all(Val::Px(1.0)),
+        ..default()
+    }),
+    BackgroundColor(|| BackgroundColor(Srgba::rgb(0.9, 0.1, 0.2).into())),
+    BorderColor(|| BorderColor(Srgba::BLACK.into()))
+)]
+struct InventoryButton;
+
 fn spawn_hud_skills(mut commands: Commands) {
     let mut observers = vec![].with_observers(SpawnInfoPopupObservers::observers());
     commands.spawn(HudSkillsPanel).with_children(|p| {
-        p.spawn(TextButton::small("I")).observe(
-            |_: Trigger<Pointer<Click>>, mut command: Commands| {
+        p.spawn(InventoryButton)
+            .with_children(|b| {
+                b.spawn(Text::new("I"));
+            })
+            .observe(|_: Trigger<Pointer<Click>>, mut command: Commands| {
                 command.trigger(ToggleInventory);
-            },
-        );
+            });
 
         let entity = p.spawn((PlayerAction::Skill1, SkillGemLocation)).id();
         observers.watch_entity(entity);
@@ -54,6 +72,6 @@ fn spawn_hud_skills(mut commands: Commands) {
     });
     commands.spawn_batch(observers);
 
-    // to force to init the update
+    // to force to update skills
     commands.trigger(PlayerEquipmentChanged);
 }
